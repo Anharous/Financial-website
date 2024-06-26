aab066545b917d25a472c32509c21cf257412102
Amirtha Lakshmi <amirtha.0726@gmail.com>
2024-06-26 23:44:43 +0530

    feat: added phase two

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,21 @@ export default function Home() {
     <div className="home-container">
        <Header/>
        <Hero/>
+       {/* phase-two */}
+       <div className="phase-two-container">
+        <div className="item-one">
+          <p>At Redtail Insurance Agency, we specialize in providing comprehensive insurance solutions tailored to meet the unique needs of businesses in Michigan. With a focus on high-risk markets, excess and surplus lines, as well as life insurance and annuities for high net worth individuals, we are committed to protecting what matters most to our clients.</p>
+        </div>
+        <div className="phase-two-right-subcontainer">
+          <div className="item-two">
+            <p>Our experienced team understands the challenges that businesses face and works tirelessly to provide personalized coverage options that offer peace of mind. Whether you're a small startup or a large corporation, we've got you covered.</p>
+          </div>
+          <div className="item-three">
+            <p>Explore our website to learn more about our services and how we can help safeguard your business.</p>
+          </div>
+        </div>
+
+       </div>
     </div>
   );
 }
