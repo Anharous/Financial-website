4a8fd5ce7b177d39a734cef82fe4c085ad4eff68
Amirtha Lakshmi <amirtha.0726@gmail.com>
2024-06-26 22:57:37 +0530

    feat: added hero page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image"
 import  "./page.css"
 import Header from './Components/Header/page'
+import Hero from './Components/Hero/page'
 
 export default function Home() {
   return (
     <div className="home-container">
        <Header/>
+       <Hero/>
     </div>
   );
 }
